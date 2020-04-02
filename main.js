10d5e2402a94330241d163760d27ae7ce2fbf59a
scammi <scammi@gmail.com>
2020-04-02 10:47:08 -0300

    Grid added

--- main.js
+++ main.js
@@ -27,12 +27,12 @@ function draw() {
 
   $('#vm-display').html(vm[vm.length - 1])
 
-  background(0);
+  background(240);
   noFill();
 
   beginShape();
 
-  stroke(255);
+  stroke(0);
   for(var i = 0; i < width; i++){
     vertex( i, vm[i])
   }
@@ -42,8 +42,24 @@ function draw() {
   if (vm.length > width){
     vm.splice(0,1);
   }
+
+  drawGrid();
+  fill(0);
 }
 
 $('button').on('click', ()=>{
   Ko = Ko + 1;
 })
+
+function drawGrid() {
+	stroke(200);
+	fill(120);
+	for (var x=-width; x < width; x+=40) {
+		line(x, -height, x, height);
+		text(x, x+1, 12);
+	}
+	for (var y=-height; y < height; y+=40) {
+		line(-width, y, width, y);
+		text(y, 1, y+12);
+	}
+}
