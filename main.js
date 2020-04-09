eb6906803e4213d7d22372faf09c0b96f5d179f8
scammi <scammi@gmail.com>
2020-04-09 14:26:24 -0300

    depolariza on click

--- main.js
+++ main.js
@@ -5,16 +5,11 @@
   var alfa = .05;
   var vm = [];
 
-  // setInterval(function() {
-  //   vm.push(vm_update());
-  // }, 1000/120);
-  function vm_update() {
-    return (61.5 * Math.log10((Ko + (alfa * No)) / (Ki + (alfa *Ni))));
-  }
-
   function setup() {
     // Sets the screen to be 720 pixels wide and 400 pixels high
     let graph = createCanvas((windowWidth * .4), (windowHeight * .4));
+    graph.mouseClicked(actionPotential);
+
     graph.parent('#chart');
   }
 
@@ -44,12 +39,27 @@
     fill(0);
   }
 
+  function vm_update() {
+    return (61.5 * Math.log10((Ko + (alfa * No)) / (Ki + (alfa *Ni))));
+  }
+
   function ion_update(){
     Ko = $('#ko-ranger').val();
     $('#ko-val').html(Ko);
 
     No = $('#no-ranger').val();
     $('#no-val').html(No);
+  }
+
+  function actionPotential(){
+    if (alfa < 0.9) {
+      for (x = 0.01; x < 4; x = x + 0.001){
+        alfa = 1 / (1 + Math.exp(-x));
+      }
+    }
+    else {
+      alfa = 0.05;
+    }
   }
 
   //Draws graph background
